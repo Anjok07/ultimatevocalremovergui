d7fef0c3bc954b35a61c8c3278a216aaad35cc62
Dilan Boskan <boskan.dilan@gmail.com>
2021-01-23 14:09:30 +0100

    Revamp of folder structure

--- tkinterdnd2/tkdnd/win64/pkgIndex.tcl
+++
@@ -1,7 +0,0 @@
-package ifneeded tkdnd 2.9.2 \
-  "source \{$dir/tkdnd.tcl\} ; \
-   tkdnd::initialise \{$dir\} libtkdnd2.9.2[info sharedlibextension] tkdnd"
-
-package ifneeded tkdnd::utils 2.9.2 \
-  "source \{$dir/tkdnd_utils.tcl\} ; \
-   package provide tkdnd::utils 2.9.2"

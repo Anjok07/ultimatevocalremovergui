020757ee7401ee68b0a6d6ddd85f86c7f5dc2a00
Anjok07 <68268275+Anjok07@users.noreply.github.com>
2021-04-09 05:12:48 -0500

    Update Drop-Track-Here-MGM-v5-Vocal_2Band-32000-BETA1.bat

--- windows-batch-files/Drop-Track-Here-MGM-v5-Vocal_2Band-32000-BETA1.bat
+++ windows-batch-files/Drop-Track-Here-MGM-v5-Vocal_2Band-32000-BETA1.bat
@@ -1,6 +1,6 @@
 @echo off
  
-set model=MGM-v5-Vocal_2Band-32000-BETA2
+set model=MGM-v5-Vocal_2Band-32000-BETA1
 cd /d %~dp0
  
 python inference.py -g 0 -m 2band_32000.json -w 352 -P models\%model%.pth -t -i %1

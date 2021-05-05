6357ebcb0c1e4cfd03a6622d09ea8cb44916d459
Anjok07 <68268275+Anjok07@users.noreply.github.com>
2021-05-05 01:23:40 -0500

    Add files via upload

--- Drag-n-Drop-CHOOSE-YOUR-MODEL.bat
+++ Drag-n-Drop-CHOOSE-YOUR-MODEL.bat
@@ -18,49 +18,49 @@ ECHO =======================================
 ECHO           Choose Your Model
 ECHO =======================================
 ECHO.
-ECHO 1. MGM-v5-2Band-32000-BETA1
-ECHO 2. MGM-v5-2Band-32000-BETA2
-ECHO 3. MGM-v5-3Band-44100-BETA
-ECHO 4. MGM-v5-4Band-44100-BETA1
-ECHO 5. MGM-v5-4Band-44100-BETA2
-ECHO 6. MGM-v5-KAROKEE-32000-BETA1
-ECHO 7. MGM-v5-KAROKEE-32000-BETA2-AGR
-ECHO 8. MGM-v5-MIDSIDE-44100-BETA1
-ECHO 9. MGM-v5-MIDSIDE-44100-BETA2
-ECHO a. MGM-v5-Vocal_2Band-32000-BETA1
-ECHO b. MGM-v5-Vocal_2Band-32000-BETA2
-ECHO c. LOFI_2band_iter5_2
-ECHO d. LOFI_2band_iter5_2
-ECHO e. HighPrecison_4band_1
-ECHO f. HighPrecison_4band_2
-ECHO g. BigLayer_4band_1.pth
-ECHO h. BigLayer_4band_2.pth
-ECHO i. BigLayer_4band_3.pth
-ECHO j. Ensemble 4Band Models (7 Models) - Final Outputs Save to Ensembled Folder!
-ECHO k. Ensemble All Models (12 Models) - Final Outputs Save to Ensembled Folder!
+ECHO a. MGM-v5-2Band-32000-BETA1
+ECHO b. MGM-v5-2Band-32000-BETA2
+ECHO c. MGM-v5-3Band-44100-BETA
+ECHO d. MGM-v5-4Band-44100-BETA1
+ECHO e. MGM-v5-4Band-44100-BETA2
+ECHO f. MGM-v5-KAROKEE-32000-BETA1
+ECHO g. MGM-v5-KAROKEE-32000-BETA2-AGR
+ECHO h. MGM-v5-MIDSIDE-44100-BETA1
+ECHO i. MGM-v5-MIDSIDE-44100-BETA2
+ECHO j. MGM-v5-Vocal_2Band-32000-BETA1
+ECHO k. MGM-v5-Vocal_2Band-32000-BETA2
+ECHO l. LOFI_2band_iter5_2
+ECHO m. LOFI_2band_iter5_2
+ECHO n. HighPrecison_4band_1
+ECHO o. HighPrecison_4band_2
+ECHO p. BigLayer_4band_1.pth
+ECHO q. BigLayer_4band_2.pth
+ECHO r. BigLayer_4band_3.pth
+ECHO s. Ensemble 4Band Models (7 Models) - Final Outputs Save to Ensembled Folder!
+ECHO t. Ensemble All Models (12 Models) - Final Outputs Save to Ensembled Folder!
 set choice=
-set /p choice=Type the number or letter associated with the option you choose and hit 'Enter': 
+set /p choice=Type the letter associated with the option you choose and hit 'Enter': 
 if not '%choice%'=='' set choice=%choice:~0,1%
-if '%choice%'=='1' goto model1
-if '%choice%'=='2' goto model2
-if '%choice%'=='3' goto model3
-if '%choice%'=='4' goto model4
-if '%choice%'=='5' goto model5
-if '%choice%'=='6' goto model6
-if '%choice%'=='7' goto model7
-if '%choice%'=='8' goto model8
-if '%choice%'=='9' goto model9
-if '%choice%'=='a' goto model10
-if '%choice%'=='b' goto model11
-if '%choice%'=='c' goto model12
-if '%choice%'=='d' goto model13
-if '%choice%'=='e' goto model14
-if '%choice%'=='f' goto model15
-if '%choice%'=='g' goto model16
-if '%choice%'=='h' goto model17
-if '%choice%'=='i' goto model18
-if '%choice%'=='j' goto model19
-if '%choice%'=='k' goto model20
+if '%choice%'=='a' goto model1
+if '%choice%'=='b' goto model2
+if '%choice%'=='c' goto model3
+if '%choice%'=='d' goto model4
+if '%choice%'=='e' goto model5
+if '%choice%'=='f' goto model6
+if '%choice%'=='g' goto model7
+if '%choice%'=='h' goto model8
+if '%choice%'=='i' goto model9
+if '%choice%'=='j' goto model10
+if '%choice%'=='k' goto model11
+if '%choice%'=='l' goto model12
+if '%choice%'=='m' goto model13
+if '%choice%'=='n' goto model14
+if '%choice%'=='o' goto model15
+if '%choice%'=='p' goto model16
+if '%choice%'=='q' goto model17
+if '%choice%'=='r' goto model18
+if '%choice%'=='s' goto model19
+if '%choice%'=='t' goto model20
 ECHO "%choice%" is not valid, try again
 ECHO.
 goto start
@@ -278,7 +278,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\3band_44100_mid.json -D -w 352 -P models\%model%.pth -t -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model MGM-v5-MIDSIDE-44100-BETA1
 set model=MGM-v5-MIDSIDE-44100-BETA1
@@ -306,7 +306,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\3band_44100_mid.json -D -w 352 -P models\%model%.pth -t -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model MGM-v5-MIDSIDE-44100-BETA2
 set model=MGM-v5-MIDSIDE-44100-BETA2
@@ -348,7 +348,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\2band_44100_lofi.json -D -w 352 -n 33966KB -P models\%model%.pth -t -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model LOFI_2band-1_33966KB
 set model=LOFI_2band-32000-1_33966KB
@@ -376,7 +376,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\2band_44100_lofi.json -D -w 352 -n 33966KB -P models\%model%.pth -t -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model LOFI_2band-2_33966KB
 set model=LOFI_2band-2_33966KB
@@ -404,7 +404,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\4band_44100.json -D -w 352 -n 123821KB -P models\%model%.pth -t -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model HighPrecison_4band_1
 set model=HighPrecison_4band_1
@@ -432,7 +432,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\4band_44100.json -D -w 352 -n 123821KB -P models\%model%.pth -t -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model HighPrecison_4band_2
 set model=HighPrecison_4band_2
@@ -486,7 +486,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\4band_44100.json -w 352 -D -n 129605KB -P models\%model%.pth -t -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model BigLayer_4band_2
 set model=BigLayer_4band_2
@@ -514,7 +514,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\4band_44100.json -w 352 -D -n 129605KB -P models\%model%.pth -t -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model 
 set model=BigLayer_4band_3
@@ -539,14 +539,14 @@ goto start
 ECHO Ensemble All 4Band Models
 cd /d %~dp0
  
-python 4Band_ens_inference.py -g 0 -D -w 352 -D -t -i %1
+python 4Band_ens_inference.py -g 0 -D -w 352 -D -s -t -i %1
 goto end
-:regular
+:default
 
 ECHO Ensemble All 4Band Models
 cd /d %~dp0
  
-python 4Band_ens_inference.py -g 0 -w 352 -D -t -i %1
+python 4Band_ens_inference.py -g 0 -w 352 -D -s -t -i %1
 goto end
 :model20
 set choice=
@@ -567,7 +567,7 @@ cd /d %~dp0
  
 python allmodels_ens_inference.py -g 0 -w 352 -D -t -i %1
 goto end
-:regular
+:default
 
 ECHO Ensemble All 44100-Models
 cd /d %~dp0
@@ -579,49 +579,49 @@ ECHO =======================================
 ECHO           Choose Your Model
 ECHO =======================================
 ECHO.
-ECHO 1. MGM-v5-2Band-32000-BETA1
-ECHO 2. MGM-v5-2Band-32000-BETA2
-ECHO 3. MGM-v5-3Band-44100-BETA
-ECHO 4. MGM-v5-4Band-44100-BETA1
-ECHO 5. MGM-v5-4Band-44100-BETA2
-ECHO 6. MGM-v5-KAROKEE-32000-BETA1
-ECHO 7. MGM-v5-KAROKEE-32000-BETA2-AGR
-ECHO 8. MGM-v5-MIDSIDE-44100-BETA1
-ECHO 9. MGM-v5-MIDSIDE-44100-BETA2
-ECHO a. MGM-v5-Vocal_2Band-32000-BETA1
-ECHO b. MGM-v5-Vocal_2Band-32000-BETA2
-ECHO c. LOFI_2band_iter5_2
-ECHO d. LOFI_2band_iter5_2
-ECHO e. HighPrecison_4band_1
-ECHO f. HighPrecison_4band_2
-ECHO g. BigLayer_4band_1.pth
-ECHO h. BigLayer_4band_2.pth
-ECHO i. BigLayer_4band_3.pth
-ECHO j. Ensemble 4Band Models (7 Models)
-ECHO k. Ensemble All Models (14 Models)
+ECHO a. MGM-v5-2Band-32000-BETA1
+ECHO b. MGM-v5-2Band-32000-BETA2
+ECHO c. MGM-v5-3Band-44100-BETA
+ECHO d. MGM-v5-4Band-44100-BETA1
+ECHO e. MGM-v5-4Band-44100-BETA2
+ECHO f. MGM-v5-KAROKEE-32000-BETA1
+ECHO g. MGM-v5-KAROKEE-32000-BETA2-AGR
+ECHO h. MGM-v5-MIDSIDE-44100-BETA1
+ECHO i. MGM-v5-MIDSIDE-44100-BETA2
+ECHO j. MGM-v5-Vocal_2Band-32000-BETA1
+ECHO k. MGM-v5-Vocal_2Band-32000-BETA2
+ECHO l. LOFI_2band_iter5_2
+ECHO m. LOFI_2band_iter5_2
+ECHO n. HighPrecison_4band_1
+ECHO o. HighPrecison_4band_2
+ECHO p. BigLayer_4band_1
+ECHO q. BigLayer_4band_2
+ECHO r. BigLayer_4band_3
+ECHO s. Ensemble 4Band Models (7 Models) - Final Outputs Save to Ensembled Folder!
+ECHO t. Ensemble All Models (12 Models) - Final Outputs Save to Ensembled Folder!
 set choice=
-set /p choice=Type the number associated with the model you would like to run and hit 'Enter': 
+set /p choice=Type the letter associated with the option you choose and hit 'Enter': 
 if not '%choice%'=='' set choice=%choice:~0,1%
-if '%choice%'=='1' goto model1
-if '%choice%'=='2' goto model2
-if '%choice%'=='3' goto model3
-if '%choice%'=='4' goto model4
-if '%choice%'=='5' goto model5
-if '%choice%'=='6' goto model6
-if '%choice%'=='7' goto model7
-if '%choice%'=='8' goto model8
-if '%choice%'=='9' goto model9
-if '%choice%'=='a' goto model10
-if '%choice%'=='b' goto model11
-if '%choice%'=='c' goto model12
-if '%choice%'=='d' goto model13
-if '%choice%'=='e' goto model14
-if '%choice%'=='f' goto model15
-if '%choice%'=='g' goto model16
-if '%choice%'=='h' goto model17
-if '%choice%'=='i' goto model18
-if '%choice%'=='j' goto model19
-if '%choice%'=='k' goto model20
+if '%choice%'=='a' goto model1
+if '%choice%'=='b' goto model2
+if '%choice%'=='c' goto model3
+if '%choice%'=='d' goto model4
+if '%choice%'=='e' goto model5
+if '%choice%'=='f' goto model6
+if '%choice%'=='g' goto model7
+if '%choice%'=='h' goto model8
+if '%choice%'=='i' goto model9
+if '%choice%'=='j' goto model10
+if '%choice%'=='k' goto model11
+if '%choice%'=='l' goto model12
+if '%choice%'=='m' goto model13
+if '%choice%'=='n' goto model14
+if '%choice%'=='o' goto model15
+if '%choice%'=='p' goto model16
+if '%choice%'=='q' goto model17
+if '%choice%'=='r' goto model18
+if '%choice%'=='s' goto model19
+if '%choice%'=='t' goto model20
 ECHO "%choice%" is not valid, try again
 ECHO.
 goto start
@@ -839,7 +839,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\3band_44100_mid.json -D -w 352 -P models\%model%.pth -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model MGM-v5-MIDSIDE-44100-BETA1
 set model=MGM-v5-MIDSIDE-44100-BETA1
@@ -867,7 +867,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\3band_44100_mid.json -D -w 352 -P models\%model%.pth -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model MGM-v5-MIDSIDE-44100-BETA2
 set model=MGM-v5-MIDSIDE-44100-BETA2
@@ -909,7 +909,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\2band_44100_lofi.json -D -w 352 -n 33966KB -P models\%model%.pth -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model LOFI_2band-1_33966KB
 set model=LOFI_2band-32000-1_33966KB
@@ -937,7 +937,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\2band_44100_lofi.json -D -w 352 -n 33966KB -P models\%model%.pth -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model LOFI_2band-2_33966KB
 set model=LOFI_2band-2_33966KB
@@ -965,7 +965,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\4band_44100.json -D -w 352 -n 123821KB -P models\%model%.pth -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model HighPrecison_4band_1
 set model=HighPrecison_4band_1
@@ -993,7 +993,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\4band_44100.json -D -w 352 -n 123821KB -P models\%model%.pth -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model HighPrecison_4band_2
 set model=HighPrecison_4band_2
@@ -1047,7 +1047,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\4band_44100.json -w 352 -D -n 129605KB -P models\%model%.pth -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model BigLayer_4band_2
 set model=BigLayer_4band_2
@@ -1075,7 +1075,7 @@ cd /d %~dp0
  
 python inference.py -g 0 -m modelparams\4band_44100.json -w 352 -D -n 129605KB -P models\%model%.pth -i %1
 goto end
-:regular
+:default
 
 ECHO Running Model 
 set model=BigLayer_4band_3
@@ -1102,7 +1102,7 @@ cd /d %~dp0
  
 python 4Band_ens_inference.py -g 0 -D -w 352 -D -i %1
 goto end
-:regular
+:default
 
 ECHO Ensemble All 4Band Models
 cd /d %~dp0
@@ -1128,7 +1128,7 @@ cd /d %~dp0
  
 python allmodels_ens_inference.py -g 0 -w 352 -D -i %1
 goto end
-:regular
+:default
 
 ECHO Ensemble All 44100-Models
 cd /d %~dp0

e6207a203cdf153c8e9d593e9137a3f15a965f21
Anjok07 <68268275+Anjok07@users.noreply.github.com>
2021-05-31 02:49:52 -0500

    Add files via upload

---
+++ Ensemble-Outputs.bat
@@ -0,0 +1,34 @@
+@echo off
+
+:start
+set choice=
+set /p choice=Are you processing instrumental outputs? [Y/N]: 
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='Y' goto inst
+if '%choice%'=='y' goto inst
+if '%choice%'=='N' goto vocals
+if '%choice%'=='n' goto vocals
+if '%choice%'=='' goto inst
+
+:inst
+ECHO Ensembling Instruments...
+set modelparam=1band_sr44100_hl512
+cd /d %~dp0
+
+python lib/spec_utils.py -a min_mag -m modelparams\%modelparam%.json %* -o "%~n3_Ensembled_Instruments"
+ECHO Complete!
+goto end
+:end
+pause
+exit
+:vocals
+ECHO Ensembling Vocals...
+set modelparam=1band_sr44100_hl512
+cd /d %~dp0
+
+python lib/spec_utils.py -a max_mag -m modelparams\%modelparam%.json %* -o "%~n3_Ensembled_Vocals"
+ECHO Complete!
+goto end
+:end
+pause
+exit

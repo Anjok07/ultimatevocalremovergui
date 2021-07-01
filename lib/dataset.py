f7cb34201f0d07ddd70e9a1e870be52d39f78f2d
aufr33 <65520685+aufr33@users.noreply.github.com>
2021-07-01 10:19:21 +0300

    Update dataset.py

--- lib/dataset.py
+++ lib/dataset.py
@@ -85,17 +85,18 @@ def augment(X, y, reduction_rate, reduction_mask, mixup_rate, mixup_alpha):
         if np.random.uniform() < reduction_rate:
             y[idx] = spec_utils.reduce_vocal_aggressively(X[idx], y[idx], reduction_mask)
 
-        if np.random.uniform() < 0.5:
-            # swap channel
-            X[idx] = X[idx, ::-1]
-            y[idx] = y[idx, ::-1]
-        if np.random.uniform() < 0.02:
-            # mono
-            X[idx] = X[idx].mean(axis=0, keepdims=True)
-            y[idx] = y[idx].mean(axis=0, keepdims=True)
-        if np.random.uniform() < 0.02:
-            # inst
-            X[idx] = y[idx]
+        if mixup_rate > 0.0:
+            if np.random.uniform() < 0.5:
+                # swap channel
+                X[idx] = X[idx, ::-1]
+                y[idx] = y[idx, ::-1]
+            if np.random.uniform() < 0.02:
+                # mono
+                X[idx] = X[idx].mean(axis=0, keepdims=True)
+                y[idx] = y[idx].mean(axis=0, keepdims=True)
+            if np.random.uniform() < 0.02:
+                # inst
+                X[idx] = y[idx]
 
         if np.random.uniform() < mixup_rate and i < len(perm) - 1:
             lam = np.random.beta(mixup_alpha, mixup_alpha)
